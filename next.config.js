597d2e3408ff76fa0316d717dd3712b14b4eb332
JAde Wii <dawlessjammin@gmail.com>
2025-09-22 17:47:32 -0700

    Remove basePath/assetPrefix from Next.js config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/jadewii.com',
-  assetPrefix: '/jadewii.com',
   images: {
     unoptimized: true,
   },
